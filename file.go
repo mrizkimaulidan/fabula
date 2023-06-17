1fb1c4da51473fb62ce3c99a92ee3a16c76abedd
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-06-17 17:14:30 +0800

    refactor: :recycle: using path.Join rather than manually concatenating using fmt

--- file.go
+++ file.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path"
 )
 
 const DIR = "stories"
@@ -37,7 +38,7 @@ func GetFile(URL string) (*http.Response, error) {
 // user information, and source reader, and save it into a folder.
 func CreateFile(file File, userInformation UserInformation, source io.Reader) (*os.File, error) {
 	// stories/{instagram-username}/{unixTime}.{extension}
-	fullPath := fmt.Sprintf("%s/%s/%s%s", DIR, userInformation.Result.User.Username, file.Name, file.Extension)
+	fullPath := path.Join(DIR, userInformation.Result.User.Username, file.Name+file.Extension)
 	createdFile, err := os.Create(fullPath)
 	if err != nil {
 		return nil, fmt.Errorf("error creating file: %s", err.Error())
